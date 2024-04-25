de7cad5606a010fe9b130839bb10a7d5e37ceb30
mrsmrynk <marius.maryniak@w-hs.de>
2024-04-25 15:37:25 +0200

    add types

--- src/utils/types.py
+++ src/utils/types.py
@@ -4,13 +4,20 @@ import numpy as np
 import numpy.typing as npt
 
 BoundingBox = tuple[int, int, int, int]
+BufferSize = int
 Coordinates = npt.NDArray[np.int32]
 EPSGCode = int
+GroundSamplingDistance = float
 TileSize = int
 XMax = int
 XMin = int
 YMax = int
 YMin = int
+
+
+class InterpolationMode(Enum):
+    BILINEAR = 'bilinear'
+    NEAREST = 'nearest'
 
 
 class GeospatialFilterMode(Enum):
