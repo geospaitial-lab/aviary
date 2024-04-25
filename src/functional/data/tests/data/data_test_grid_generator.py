30dfe9decefd36ff86277c8f8454349695912883
mrsmrynk <marius.maryniak@w-hs.de>
2024-04-25 10:57:18 +0200

    change indentation

--- src/functional/data/tests/data/data_test_grid_generator.py
+++ src/functional/data/tests/data/data_test_grid_generator.py
@@ -2,10 +2,12 @@ import numpy as np
 from shapely.geometry import box
 
 data_test_compute_coordinates = [
-    ((-128, -128, 128, 128),
-     128,
-     True,
-     np.array([[-128, -128], [0, -128], [-128, 0], [0, 0]], dtype=np.int32)),
+    (
+        (-128, -128, 128, 128),
+        128,
+        True,
+        np.array([[-128, -128], [0, -128], [-128, 0], [0, 0]], dtype=np.int32),
+    ),
 ]
 
 data_test__quantize_coordinates = [
@@ -21,10 +23,18 @@ data_test__quantize_coordinates = [
 ]
 
 data_test__generate_polygons = [
-    (np.array([[0, 0]], dtype=np.int32),
-     256,
-     [box(0, 0, 256, 256)]),
-    (np.array([[-256, -256], [0, 0], [256, 256]], dtype=np.int32),
-     256,
-     [box(-256, -256, 0, 0), box(0, 0, 256, 256), box(256, 256, 512, 512)]),
+    (
+        np.array([[0, 0]], dtype=np.int32),
+        256,
+        [box(0, 0, 256, 256)],
+    ),
+    (
+        np.array([[-256, -256], [0, 0], [256, 256]], dtype=np.int32),
+        256,
+        [
+            box(-256, -256, 0, 0),
+            box(0, 0, 256, 256),
+            box(256, 256, 512, 512),
+        ],
+    ),
 ]
