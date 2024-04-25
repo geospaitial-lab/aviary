6878c8188f60f40fe5377b69a84447e6b8983032
Marius Maryniak <marius.maryniak@w-hs.de>
2024-04-25 11:25:21 +0000

    test(`GridGenerator`): add test case (#21)

--- src/functional/data/tests/data/data_test_grid_generator.py
+++ src/functional/data/tests/data/data_test_grid_generator.py
@@ -2,8 +2,16 @@ import numpy as np
 from shapely.geometry import box
 
 data_test_compute_coordinates = [
+    # test case 1: bounding_box is not quantized
     (
-        (-128, -128, 128, 128),
+        (-127, -127, 128, 128),
+        128,
+        False,
+        np.array([[-127, -127], [1, -127], [-127, 1], [1, 1]], dtype=np.int32),
+    ),
+    # test case 2: bounding_box is quantized
+    (
+        (-127, -127, 128, 128),
         128,
         True,
         np.array([[-128, -128], [0, -128], [-128, 0], [0, 0]], dtype=np.int32),
@@ -11,30 +19,35 @@ data_test_compute_coordinates = [
 ]
 
 data_test__quantize_coordinates = [
+    # test case 1: x_min < 0 and y_min < 0, x_min and y_min are not divisible by tile_size
     (-129, -129, 128, (-256, -256)),
+    # test case 2: x_min < 0 and y_min < 0, x_min and y_min are divisible by tile_size
     (-128, -128, 128, (-128, -128)),
+    # test case 3: x_min < 0 and y_min < 0, x_min and y_min are not divisible by tile_size
     (-127, -127, 128, (-128, -128)),
+    # test case 4: x_min < 0 and y_min < 0, x_min and y_min are not divisible by tile_size
     (-1, -1, 128, (-128, -128)),
+    # test case 5: x_min = 0 and y_min = 0, x_min and y_min are divisible by tile_size
     (0, 0, 128, (0, 0)),
+    # test case 6: x_min > 0 and y_min > 0, x_min and y_min are not divisible by tile_size
     (1, 1, 128, (0, 0)),
+    # test case 7: x_min > 0 and y_min > 0, x_min and y_min are not divisible by tile_size
     (127, 127, 128, (0, 0)),
+    # test case 8: x_min > 0 and y_min > 0, x_min and y_min are divisible by tile_size
     (128, 128, 128, (128, 128)),
+    # test case 9: x_min > 0 and y_min > 0, x_min and y_min are not divisible by tile_size
     (129, 129, 128, (128, 128)),
 ]
 
 data_test__generate_polygons = [
     (
-        np.array([[0, 0]], dtype=np.int32),
-        256,
-        [box(0, 0, 256, 256)],
-    ),
-    (
-        np.array([[-256, -256], [0, 0], [256, 256]], dtype=np.int32),
-        256,
+        np.array([[-128, -128], [0, -128], [-128, 0], [0, 0]], dtype=np.int32),
+        128,
         [
-            box(-256, -256, 0, 0),
-            box(0, 0, 256, 256),
-            box(256, 256, 512, 512),
+            box(-128, -128, 0, 0),
+            box(0, -128, 128, 0),
+            box(-128, 0, 0, 128),
+            box(0, 0, 128, 128),
         ],
     ),
 ]
