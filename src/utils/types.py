93750b15bfe6d93f3db6da46e986151d3cfceea1
mrsmrynk <marius.maryniak@w-hs.de>
2024-04-26 10:51:38 +0200

    add type conversion for interpolation mode

--- src/utils/types.py
+++ src/utils/types.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 import numpy as np
 import numpy.typing as npt
+import rasterio as rio
 
 BoundingBox = tuple[int, int, int, int]
 BufferSize = int
@@ -18,6 +19,18 @@ YMin = int
 class InterpolationMode(Enum):
     BILINEAR = 'bilinear'
     NEAREST = 'nearest'
+
+    def to_rio(self):
+        """
+        | Converts the interpolation mode to the rasterio resampling mode.
+
+        :return: rasterio resampling mode
+        """
+        mapping = {
+            InterpolationMode.BILINEAR: rio.enums.Resampling.bilinear,
+            InterpolationMode.NEAREST: rio.enums.Resampling.nearest,
+        }
+        return mapping[self]
 
 
 class GeospatialFilterMode(Enum):
