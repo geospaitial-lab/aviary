bb54bdaa6553ac4baaa194d003ddaa683d768108
mrsmrynk <marius.maryniak@w-hs.de>
2024-05-02 21:51:08 +0200

    edit docstrings

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -12,6 +12,11 @@ from src.utils.types import (
 
 
 class Dataset(torch.utils.data.Dataset):
+    """Dataset
+
+    A dataset is an iterable that returns data for each tile by calling the data fetcher and data preprocessor.
+    The dataset is used by the dataloader to fetch and preprocess data for each batch.
+    """
 
     def __init__(
         self,
@@ -20,19 +25,20 @@ class Dataset(torch.utils.data.Dataset):
         coordinates: Coordinates,
     ) -> None:
         """
-        :param data_fetcher: data fetcher
-        :param data_preprocessor: data preprocessor
-        :param coordinates: coordinates (x_min, y_min) of each tile
+        Parameters:
+            data_fetcher: data fetcher
+            data_preprocessor: data preprocessor
+            coordinates: coordinates (x_min, y_min) of each tile
         """
         self.data_fetcher = data_fetcher
         self.data_preprocessor = data_preprocessor
         self.coordinates = coordinates
 
     def __len__(self) -> int:
-        """
-        | Returns the number of tiles.
+        """Computes the number of tiles.
 
-        :return: number of tiles
+        Returns:
+            number of tiles
         """
         return get_length(
             coordinates=self.coordinates,
@@ -42,11 +48,13 @@ class Dataset(torch.utils.data.Dataset):
         self,
         index: int,
     ) -> torch.Tensor:
-        """
-        | Returns the data.
+        """Fetches and preprocesses data given the index of the tile.
 
-        :param index: index of the tile
-        :return: data
+        Parameters:
+            index: index of the tile
+
+        Returns:
+            data
         """
         return get_item(
             coordinates=self.coordinates,
