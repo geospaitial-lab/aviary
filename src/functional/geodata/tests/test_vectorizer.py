ef3ab1d1c757ec960f4cd2e23e53c75684de0c81
mrsmrynk <marius.maryniak@w-hs.de>
2024-05-15 16:44:55 +0200

    add tests for functions for Vectorizer

---
+++ src/functional/geodata/tests/test_vectorizer.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from unittest.mock import patch
+
+import dask
+import numpy as np
+import pytest
+
+from src.functional.geodata.vectorizer import (
+    _vectorizer_element,
+)
+
+
+@pytest.mark.skip(reason='Not implemented')
+def test_vectorizer() -> None:
+    pass
+
+
+@patch('src.functional.geodata.vectorizer._export_gdf')
+@patch('src.functional.geodata.vectorizer._vectorize_preds')
+def test__vectorizer_element(
+    mocked__vectorize_preds,
+    mocked__export_gdf,
+) -> None:
+    preds = np.ones(shape=(640, 640), dtype=np.uint8)
+    x_min = -128
+    y_min = -128
+    path = Path('test')
+    tile_size = 128
+    ground_sampling_distance = .2
+    epsg_code = 25832
+    field_name = 'class'
+    ignore_background_class = True
+    expected_gdf = 'expected_gdf'
+    mocked__vectorize_preds.return_value = expected_gdf
+    dask.compute(
+        _vectorizer_element(
+            preds=preds,
+            x_min=x_min,
+            y_min=y_min,
+            path=path,
+            tile_size=tile_size,
+            ground_sampling_distance=ground_sampling_distance,
+            epsg_code=epsg_code,
+            field_name=field_name,
+            ignore_background_class=ignore_background_class,
+        )
+    )
+
+    mocked__vectorize_preds.assert_called_once_with(
+        preds=preds,
+        x_min=x_min,
+        y_min=y_min,
+        tile_size=tile_size,
+        ground_sampling_distance=ground_sampling_distance,
+        epsg_code=epsg_code,
+        field_name=field_name,
+        ignore_background_class=ignore_background_class,
+    )
+    mocked__export_gdf.assert_called_once_with(
+        gdf=expected_gdf,
+        x_min=x_min,
+        y_min=y_min,
+        path=path,
+    )
