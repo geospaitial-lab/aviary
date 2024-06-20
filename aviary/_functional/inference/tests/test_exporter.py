88b22a6cb95d89cca5a5539c59cecb84583df4fc
mrsmrynk <marius.maryniak@w-hs.de>
2024-06-20 20:39:18 +0200

    add mode

--- aviary/_functional/inference/tests/test_exporter.py
+++ aviary/_functional/inference/tests/test_exporter.py
@@ -5,7 +5,9 @@ import dask
 import numpy as np
 import pytest
 
-from aviary._functional.inference.exporter import _segmentation_exporter_element
+from aviary._functional.inference.exporter import _segmentation_exporter_task
+# noinspection PyProtectedMember
+from aviary._utils.types import SegmentationExporterMode
 
 
 @pytest.mark.skip(reason='Not implemented')
@@ -15,7 +17,7 @@ def test_segmentation_exporter() -> None:
 
 @patch('aviary._functional.inference.exporter._export_gdf')
 @patch('aviary._functional.inference.exporter._vectorize_preds')
-def test__segmentation_exporter_element(
+def test__segmentation_exporter_task(
     mocked__vectorize_preds,
     mocked__export_gdf,
 ) -> None:
@@ -28,10 +30,12 @@ def test__segmentation_exporter_element(
     epsg_code = 25832
     field_name = 'class'
     ignore_background_class = True
+    gpkg_name = 'output.gpkg'
+    mode = SegmentationExporterMode.GPKG
     expected_gdf = 'expected_gdf'
     mocked__vectorize_preds.return_value = expected_gdf
     dask.compute(
-        _segmentation_exporter_element(
+        _segmentation_exporter_task(
             preds=preds,
             x_min=x_min,
             y_min=y_min,
@@ -41,6 +45,8 @@ def test__segmentation_exporter_element(
             epsg_code=epsg_code,
             field_name=field_name,
             ignore_background_class=ignore_background_class,
+            gpkg_name=gpkg_name,
+            mode=mode,
         )
     )
 
@@ -56,14 +62,26 @@ def test__segmentation_exporter_element(
     )
     mocked__export_gdf.assert_called_once_with(
         gdf=expected_gdf,
+        path=path,
         x_min=x_min,
         y_min=y_min,
-        path=path,
+        gpkg_name=gpkg_name,
+        mode=mode,
     )
 
 
 @pytest.mark.skip(reason='Not implemented')
 def test__export_gdf() -> None:
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented')
+def test__export_gdf_feather() -> None:
+    pass
+
+
+@pytest.mark.skip(reason='Not implemented')
+def test__export_gdf_gpkg() -> None:
     pass
 
 
