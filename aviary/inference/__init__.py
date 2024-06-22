220f04ff46fe60ae36b146fcfc1e2c70a5f5764a
mrsmrynk <marius.maryniak@w-hs.de>
2024-06-22 12:51:25 +0200

    add Model

--- aviary/inference/__init__.py
+++ aviary/inference/__init__.py
@@ -2,8 +2,16 @@ from .exporter import (
     Exporter,
     SegmentationExporter,
 )
+from .model import (
+    Model,
+    ONNXSegmentationModel,
+    SegmentationModel,
+)
 
 __all__ = [
     'Exporter',
+    'Model',
+    'ONNXSegmentationModel',
     'SegmentationExporter',
+    'SegmentationModel',
 ]
