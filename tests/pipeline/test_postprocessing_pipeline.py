88e7f0ae82a32c9c3264868eb3e36e7b6ab772b2
mrsmrynk <marius.maryniak@w-hs.de>
2024-08-05 14:36:28 +0200

    add config types to test_globals

--- tests/pipeline/test_postprocessing_pipeline.py
+++ tests/pipeline/test_postprocessing_pipeline.py
@@ -4,12 +4,19 @@ import aviary.pipeline.postprocessing_pipeline
 def test_globals() -> None:
     class_names = [
         'ClipPostprocessor',
+        'ClipPostprocessorConfig',
         'CompositePostprocessor',
+        'CompositePostprocessorConfig',
         'FieldNamePostprocessor',
+        'FieldNamePostprocessorConfig',
         'FillPostprocessor',
+        'FillPostprocessorConfig',
         'SievePostprocessor',
+        'SievePostprocessorConfig',
         'SimplifyPostprocessor',
+        'SimplifyPostprocessorConfig',
         'ValuePostprocessor',
+        'ValuePostprocessorConfig',
     ]
 
     for class_name in class_names:
