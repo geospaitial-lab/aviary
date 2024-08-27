570c5631debc21d91041963510d8c1fef6ac760c
mrsmrynk <marius.maryniak@w-hs.de>
2024-08-27 17:25:21 +0200

    add tests for WMSFetcher

--- tests/data/test_data_fetcher.py
+++ tests/data/test_data_fetcher.py
@@ -2,10 +2,15 @@ from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 # noinspection PyProtectedMember
-from aviary._utils.types import InterpolationMode
+from aviary._utils.types import (
+    InterpolationMode,
+    WMSVersion,
+)
 from aviary.data.data_fetcher import (
     VRTFetcher,
     VRTFetcherConfig,
+    WMSFetcher,
+    WMSFetcherConfig,
 )
 
 
@@ -82,5 +87,110 @@ def test_vrt_fetcher_call(
         buffer_size=vrt_fetcher.buffer_size,
         drop_channels=vrt_fetcher.drop_channels,
         fill_value=vrt_fetcher._FILL_VALUE,
+    )
+    assert data == expected
+
+
+def test_wms_fetcher_init() -> None:
+    url = 'https://test.com'
+    version = WMSVersion.V1_3_0
+    layer = 'test_layer'
+    epsg_code = 25832
+    response_format = 'image/png'
+    tile_size = 128
+    ground_sampling_distance = .2
+    style = None
+    buffer_size = 0
+    drop_channels = None
+    wms_fetcher = WMSFetcher(
+        url=url,
+        version=version,
+        layer=layer,
+        epsg_code=epsg_code,
+        response_format=response_format,
+        tile_size=tile_size,
+        ground_sampling_distance=ground_sampling_distance,
+        style=style,
+        buffer_size=buffer_size,
+        drop_channels=drop_channels,
+    )
+
+    assert wms_fetcher.url == url
+    assert wms_fetcher.version == version
+    assert wms_fetcher.layer == layer
+    assert wms_fetcher.epsg_code == epsg_code
+    assert wms_fetcher.response_format == response_format
+    assert wms_fetcher.tile_size == tile_size
+    assert wms_fetcher.ground_sampling_distance == ground_sampling_distance
+    assert wms_fetcher.style == style
+    assert wms_fetcher.buffer_size == buffer_size
+    assert wms_fetcher.drop_channels == drop_channels
+
+
+def test_wms_fetcher_from_config() -> None:
+    url = 'https://test.com'
+    version = WMSVersion.V1_3_0
+    layer = 'test_layer'
+    epsg_code = 25832
+    response_format = 'image/png'
+    tile_size = 128
+    ground_sampling_distance = .2
+    style = None
+    buffer_size = 0
+    drop_channels = None
+    wms_fetcher_config = WMSFetcherConfig(
+        url=url,
+        version=version,
+        layer=layer,
+        epsg_code=epsg_code,
+        response_format=response_format,
+        tile_size=tile_size,
+        ground_sampling_distance=ground_sampling_distance,
+        style=style,
+        buffer_size=buffer_size,
+        drop_channels=drop_channels,
+    )
+    wms_fetcher = WMSFetcher.from_config(wms_fetcher_config)
+
+    assert wms_fetcher.url == url
+    assert wms_fetcher.version == version
+    assert wms_fetcher.layer == layer
+    assert wms_fetcher.epsg_code == epsg_code
+    assert wms_fetcher.response_format == response_format
+    assert wms_fetcher.tile_size == tile_size
+    assert wms_fetcher.ground_sampling_distance == ground_sampling_distance
+    assert wms_fetcher.style == style
+    assert wms_fetcher.buffer_size == buffer_size
+    assert wms_fetcher.drop_channels == drop_channels
+
+
+@patch('aviary.data.data_fetcher.wms_fetcher')
+def test_wms_fetcher_call(
+    mocked_wms_fetcher: MagicMock,
+    wms_fetcher: WMSFetcher,
+) -> None:
+    x_min = -128
+    y_min = -128
+    expected = 'expected'
+    mocked_wms_fetcher.return_value = expected
+    data = wms_fetcher(
+        x_min=x_min,
+        y_min=y_min,
+    )
+
+    mocked_wms_fetcher.assert_called_once_with(
+        x_min=x_min,
+        y_min=y_min,
+        url=wms_fetcher.url,
+        version=wms_fetcher.version,
+        layer=wms_fetcher.layer,
+        epsg_code=wms_fetcher.epsg_code,
+        response_format=wms_fetcher.response_format,
+        tile_size=wms_fetcher.tile_size,
+        ground_sampling_distance=wms_fetcher.ground_sampling_distance,
+        style=wms_fetcher.style,
+        buffer_size=wms_fetcher.buffer_size,
+        drop_channels=wms_fetcher.drop_channels,
+        fill_value=wms_fetcher._FILL_VALUE,
     )
     assert data == expected
