32e96dd18e4fb78a1ec93d7af4a239fcfc0b37cf
mrsmrynk <marius.maryniak@w-hs.de>
2024-10-11 11:02:46 +0200

    add noopener noreferrer to links

--- docs/javascripts/open_links_in_new_tab.js
+++ docs/javascripts/open_links_in_new_tab.js
@@ -4,6 +4,7 @@ const setLinksToOpenInNewTab = () => {
   links.forEach(link => {
     if (link.hostname !== window.location.hostname) {
       link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
     }
   });
 };
