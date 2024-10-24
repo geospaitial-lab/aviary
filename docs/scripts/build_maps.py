e34cafe692b3eb89b27c66f15152aacedaf17777
mrsmrynk <marius.maryniak@w-hs.de>
2024-10-24 16:14:20 +0200

    add build_map for how to use the bounding box

--- docs/scripts/build_maps.py
+++ docs/scripts/build_maps.py
@@ -1,18 +1,300 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from pathlib import Path
+from pathlib import Path
 
 import folium
 import geopandas as gpd
+from shapely.geometry import box
+
+import aviary
 
 
 def build_maps() -> None:
     """Builds the maps for the docs."""
-    pass  # noqa: PIE790
+    build_bounding_box_map()
+    build_bounding_box_setter_map()
+    build_bounding_box_from_gdf_map()
+    build_bounding_box_from_gdf_districts_map()
+    build_bounding_box_buffer_1_map()
+    build_bounding_box_buffer_2_map()
+    build_bounding_box_quantize_map()
+    # build_process_area_map()  # noqa: ERA001
+    # build_process_area_setter_map()  # noqa: ERA001
+    # build_process_area_from_bounding_box_map()  # noqa: ERA001
+    # build_process_area_from_bounding_box_tile_size_map()  # noqa: ERA001
+    # build_process_area_from_bounding_box_quantize_map()  # noqa: ERA001
+    # build_process_area_from_gdf_map()  # noqa: ERA001
+    # build_process_area_from_gdf_districts_map()  # noqa: ERA001
+    # build_process_area_add_map()  # noqa: ERA001
+    # build_process_area_sub_map()  # noqa: ERA001
+    # build_process_area_and_map()  # noqa: ERA001
+    # build_process_area_append_map()  # noqa: ERA001
+    # build_process_area_filter_difference_map()  # noqa: ERA001
+    # build_process_area_filter_intersection_map()  # noqa: ERA001
+
+
+def build_bounding_box_map() -> None:
+    """Builds the bounding_box map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363084,
+        y_min=5715326,
+        x_max=363340,
+        y_max=5715582,
+    )
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [bounding_box_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_bounding_box_setter_map() -> None:
+    """Builds the bounding_box_setter map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363148,
+        y_min=5715390,
+        x_max=363276,
+        y_max=5715518,
+    )
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [bounding_box_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_setter.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_bounding_box_from_gdf_map() -> None:
+    """Builds the bounding_box_from_gdf map."""
+    gdf = gpd.GeoDataFrame(
+        geometry=[box(363084, 5715326, 363340, 5715582)],
+        crs='EPSG:25832',
+    )
+
+    bounding_box = aviary.BoundingBox.from_gdf(gdf=gdf)
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    gdf_style = {
+        'fillOpacity': 0.,
+        'color': '#FF595E',
+        'weight': 2,
+    }
+    gdf_layer = Layer(
+        gdf=gdf,
+        style=gdf_style,
+    )
+
+    layers = [bounding_box_layer, gdf_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_from_gdf.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_bounding_box_from_gdf_districts_map() -> None:
+    """Builds the bounding_box_from_gdf_districts map."""
+    gdf = gpd.read_file('docs/how_to_guides/api/data/districts.geojson')
+
+    bounding_box = aviary.BoundingBox.from_gdf(gdf=gdf)
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    gdf_style = {
+        'fillOpacity': 0.,
+        'color': '#FF595E',
+        'weight': 2,
+    }
+    gdf_layer = Layer(
+        gdf=gdf,
+        style=gdf_style,
+    )
+
+    layers = [bounding_box_layer, gdf_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_from_gdf_districts.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+        zoom_start=12,
+    )
+
+
+def build_bounding_box_buffer_1_map() -> None:
+    """Builds the bounding_box_buffer_1 map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363084,
+        y_min=5715326,
+        x_max=363340,
+        y_max=5715582,
+    )
+
+    buffered_bounding_box = bounding_box.buffer(buffer_size=64)
+    buffered_bounding_box_gdf = buffered_bounding_box.to_gdf(25832)
+    buffered_bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    buffered_bounding_box_layer = Layer(
+        gdf=buffered_bounding_box_gdf,
+        style=buffered_bounding_box_style,
+    )
+
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': 0,
+        'color': '#FF595E',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [buffered_bounding_box_layer, bounding_box_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_buffer_1.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_bounding_box_buffer_2_map() -> None:
+    """Builds the bounding_box_buffer_2 map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363084,
+        y_min=5715326,
+        x_max=363340,
+        y_max=5715582,
+    )
+
+    buffered_bounding_box = bounding_box.buffer(buffer_size=-64)
+    buffered_bounding_box_gdf = buffered_bounding_box.to_gdf(25832)
+    buffered_bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    buffered_bounding_box_layer = Layer(
+        gdf=buffered_bounding_box_gdf,
+        style=buffered_bounding_box_style,
+    )
+
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': 0,
+        'color': '#FF595E',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [buffered_bounding_box_layer, bounding_box_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_buffer_2.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_bounding_box_quantize_map() -> None:
+    """Builds the bounding_box_quantize map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363084,
+        y_min=5715326,
+        x_max=363340,
+        y_max=5715582,
+    )
+
+    quantized_bounding_box = bounding_box.quantize(value=128)
+    quantized_bounding_box_gdf = quantized_bounding_box.to_gdf(25832)
+    quantized_bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+
+    quantized_bounding_box_layer = Layer(
+        gdf=quantized_bounding_box_gdf,
+        style=quantized_bounding_box_style,
+    )
+
+    bounding_box_gdf = bounding_box.to_gdf(25832)
+    bounding_box_style = {
+        'fillOpacity': 0,
+        'color': '#FF595E',
+        'weight': 2,
+    }
+
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [quantized_bounding_box_layer, bounding_box_layer]
+    path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps' / 'bounding_box_quantize.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
 
 
 def build_map(
