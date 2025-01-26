e9dce3f7193da46a528f6a89880b1da8bbaf9b0c
mrsmrynk <marius.maryniak@w-hs.de>
2025-01-26 18:39:24 +0100

    add getitem

--- aviary/core/tiles.py
+++ aviary/core/tiles.py
@@ -273,3 +273,30 @@ class Tiles(Iterable[tuple[Channel | str, npt.NDArray]]):
             number of channels
         """
         return len(self._data)
+
+    def __getitem__(
+        self,
+        channel: Channel | str,
+    ) -> npt.NDArray:
+        """Returns the channel data.
+
+        Parameters:
+            channel: channel
+
+        Returns:
+            channel data
+
+        Raises:
+            AviaryUserError: Invalid channel (the channel is not available)
+        """
+        if isinstance(channel, str) and channel in self._built_in_channels:
+            channel = Channel(channel)
+
+        if channel not in self.channels:
+            message = (
+                'Invalid channel! '
+                f'The {channel} channel is not available.'
+            )
+            raise AviaryUserError(message)
+
+        return self._data[channel]
