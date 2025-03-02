1a76c7ff477475350f0574ad46eb63852ee7a152
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-02 21:04:47 +0100

    rename ProcessArea to Grid

--- aviary/inference/tile_set.py
+++ aviary/inference/tile_set.py
@@ -3,22 +3,21 @@ from collections.abc import (
     Iterator,
 )
 
-from aviary.core.process_area import ProcessArea
+from aviary.core.grid import Grid
 from aviary.core.tiles import Tile
 from aviary.inference.tile_fetcher import TileFetcher
 
 
 class TileSet(Iterable[Tile]):
-    """A tile set is an iterable that yields a tile for each coordinates in the process area
-    by calling the tile fetcher.
+    """A tile set is an iterable that yields a tile for each coordinates in the grid by calling the tile fetcher.
 
     Examples:
-        Assume the process area and the tile fetcher are already created.
+        Assume the grid and the tile fetcher are already created.
 
         You can create a tile set and iterate over the tiles.
 
         >>> tile_set = TileSet(
-        ...     process_area=process_area,
+        ...     grid=grid,
         ...     tile_fetcher=tile_fetcher,
         ... )
         ...
@@ -28,15 +27,15 @@ class TileSet(Iterable[Tile]):
 
     def __init__(
         self,
-        process_area: ProcessArea,
+        grid: Grid,
         tile_fetcher: TileFetcher,
     ) -> None:
         """
         Parameters:
-            process_area: Process area
+            grid: Grid
             tile_fetcher: Tile fetcher
         """
-        self._process_area = process_area
+        self._grid = grid
         self._tile_fetcher = tile_fetcher
 
     def __len__(self) -> int:
@@ -45,7 +44,7 @@ class TileSet(Iterable[Tile]):
         Returns:
             Number of tiles
         """
-        return len(self._process_area)
+        return len(self._grid)
 
     def __getitem__(
         self,
@@ -59,7 +58,7 @@ class TileSet(Iterable[Tile]):
         Returns:
             Tile
         """
-        coordinates = self._process_area[index]
+        coordinates = self._grid[index]
         return self._tile_fetcher(coordinates=coordinates)
 
     def __iter__(self) -> Iterator[Tile]:
