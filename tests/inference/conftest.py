ed38690b482c01cb09b87dab64f0053c793be16f
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-09 16:47:38 +0100

    remove Exporter

--- tests/inference/conftest.py
+++ tests/inference/conftest.py
@@ -8,7 +8,6 @@ from aviary.core.enums import (
     InterpolationMode,
     WMSVersion,
 )
-from aviary.inference.exporter import SegmentationExporter
 from aviary.inference.tile_fetcher import (
     CompositeFetcher,
     TileFetcher,
@@ -27,24 +26,6 @@ def composite_fetcher() -> CompositeFetcher:
     num_workers = 1
     return CompositeFetcher(
         tile_fetchers=tile_fetchers,
-        num_workers=num_workers,
-    )
-
-
-@pytest.fixture(scope='session')
-def segmentation_exporter() -> SegmentationExporter:
-    path = Path('test')
-    tile_size = 128
-    ground_sampling_distance = .2
-    epsg_code = 25832
-    field_name = 'class'
-    num_workers = 4
-    return SegmentationExporter(
-        path=path,
-        tile_size=tile_size,
-        ground_sampling_distance=ground_sampling_distance,
-        epsg_code=epsg_code,
-        field_name=field_name,
         num_workers=num_workers,
     )
 
