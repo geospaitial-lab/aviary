60138c8bcc26f63a766c426c839f6bb4ce7581ec
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-11 12:27:42 +0100

    add tests

---
+++ tests/_utils/strategies/strategies_test_validators.py
@@ -0,0 +1,16 @@
+import re
+
+import hypothesis.strategies as st
+
+from aviary.core.enums import ChannelName
+
+invalid_channel_names = st.text().filter(lambda x: not re.match(r'^[A-Za-z_]+$', x))
+
+valid_channel_names = st.one_of(
+    st.just(list(ChannelName)),
+    st.text(
+        alphabet='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
+        min_size=1,
+    ),
+    st.none(),
+)
