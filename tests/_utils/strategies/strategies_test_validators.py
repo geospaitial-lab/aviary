3532e5dc57aefb9531ce742856fb4c3145e23a6f
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-11 12:41:16 +0100

    fix strategy

--- tests/_utils/strategies/strategies_test_validators.py
+++ tests/_utils/strategies/strategies_test_validators.py
@@ -7,7 +7,7 @@ from aviary.core.enums import ChannelName
 invalid_channel_names = st.text().filter(lambda x: not re.match(r'^[A-Za-z_]+$', x))
 
 valid_channel_names = st.one_of(
-    st.just(list(ChannelName)),
+    st.sampled_from(list(ChannelName)),
     st.text(
         alphabet='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_',
         min_size=1,
