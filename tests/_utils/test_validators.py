ea8b1a01f9861b4c89aaa8314f7dfbc0bb7996d5
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-12 20:52:47 +0100

    refactor

--- tests/_utils/test_validators.py
+++ tests/_utils/test_validators.py
@@ -4,7 +4,7 @@ import hypothesis
 import pytest
 
 # noinspection PyProtectedMember
-from aviary._utils.validators import _validate_channel_name
+from aviary._utils.validators import validate_channel_name
 from aviary.core.enums import ChannelName
 from aviary.core.exceptions import AviaryUserError
 from tests._utils.strategies.strategies_test_validators import (
@@ -14,17 +14,17 @@ from tests._utils.strategies.strategies_test_validators import (
 
 
 @hypothesis.given(channel_name=valid_channel_names)
-def test__validate_channel_name_valid(
+def test_validate_channel_name_valid(
     channel_name: ChannelName | str | None,
 ) -> None:
-    _validate_channel_name(channel_name=channel_name)
+    validate_channel_name(channel_name=channel_name)
 
 
 @hypothesis.given(channel_name=invalid_channel_names)
-def test__validate_channel_name_invalid(
+def test_validate_channel_name_invalid(
     channel_name: str,
 ) -> None:
     message = re.escape('Invalid channel_name! The channel name must contain only characters and underscores.')
 
     with pytest.raises(AviaryUserError, match=message):
-        _validate_channel_name(channel_name=channel_name)
+        validate_channel_name(channel_name=channel_name)
