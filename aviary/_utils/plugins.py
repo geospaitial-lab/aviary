ce2311b6fdccc48c51698a204f204edb8ba45f3f
mrsmrynk <marius.maryniak@w-hs.de>
2025-03-17 19:20:14 +0100

    fix

--- aviary/_utils/plugins.py
+++ aviary/_utils/plugins.py
@@ -4,18 +4,18 @@ from pathlib import Path
 
 
 def register_plugins(
-    plugins_dir_path: Path,
+    plugins_dir_path: Path = Path('plugins'),
 ) -> None:
     """Registers plugins.
 
     Parameters:
         plugins_dir_path: Path to the plugins directory
     """
-    sys.path.append(str(plugins_dir_path))
+    sys.path.append(str(plugins_dir_path.parent))
 
     try:
         for plugin_path in plugins_dir_path.iterdir():
             if plugin_path.is_file() and plugin_path.suffix == '.py':
                 importlib.import_module(f'{plugins_dir_path.name}.{plugin_path.stem}')
     finally:
-        sys.path.remove(str(plugins_dir_path))
+        sys.path.remove(str(plugins_dir_path.parent))
