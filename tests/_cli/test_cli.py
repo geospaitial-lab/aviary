2df0365145b5d4b2f3777d7b424cdfbfadae9ee8
mrsmrynk <marius.maryniak@w-hs.de>
2025-05-09 15:42:39 +0200

    add tests

---
+++ tests/_cli/test_cli.py
@@ -0,0 +1,41 @@
+from typing import TYPE_CHECKING
+
+from typer.testing import CliRunner
+
+if TYPE_CHECKING:
+    from click.testing import Result
+
+from aviary._cli.cli import app
+
+
+def test_no_args(
+    runner: CliRunner,
+) -> None:
+    result: Result = runner.invoke(
+        app=app,
+        args=[],
+    )
+
+    assert result.exit_code == 0
+
+
+def test_help(
+    runner: CliRunner,
+) -> None:
+    result: Result = runner.invoke(
+        app=app,
+        args=['--help'],
+    )
+
+    assert result.exit_code == 0
+
+
+def test_version(
+    runner: CliRunner,
+) -> None:
+    result: Result = runner.invoke(
+        app=app,
+        args=['--version'],
+    )
+
+    assert result.exit_code == 0
