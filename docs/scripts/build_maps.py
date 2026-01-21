0a866e83f5bf6ba52169f59ff70c92c515f3b5e1
mrsmrynk <marius.maryniak@w-hs.de>
2026-01-21 17:25:44 +0100

    add build maps script

---
+++ docs/scripts/build_maps.py
@@ -0,0 +1,141 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+
+import folium
+import geopandas as gpd
+
+import aviary
+
+
+def build_maps() -> None:
+    """Builds the maps for the docs."""
+    build_bounding_box_map()
+
+
+def build_bounding_box_map() -> None:
+    """Builds the bounding_box map."""
+    bounding_box = aviary.BoundingBox(
+        x_min=363084,
+        y_min=5715326,
+        x_max=363340,
+        y_max=5715582,
+    )
+
+    bounding_box_gdf = bounding_box.to_gdf(epsg_code=25832)
+    bounding_box_style = {
+        'fillOpacity': .2,
+        'color': 'black',
+        'weight': 2,
+    }
+    bounding_box_layer = Layer(
+        gdf=bounding_box_gdf,
+        style=bounding_box_style,
+    )
+
+    layers = [bounding_box_layer]
+    dir_path = Path(__file__).parents[1] / 'how_to_guides' / 'api' / 'maps'
+    path = dir_path / 'bounding_box.html'
+
+    build_map(
+        layers=layers,
+        path=path,
+    )
+
+
+def build_map(
+    layers: list[Layer],
+    path: Path,
+    zoom_start: int = 16,
+) -> None:
+    """Builds a map.
+
+    Parameters:
+        layers: Layers
+        path: Path
+        zoom_start: Zoom start
+    """
+    location = compute_location(
+        layers=layers,
+    )
+
+    folium_map = folium.Map(
+        location=location,
+        zoom_start=zoom_start,
+        tiles=None,
+    )
+
+    osm_layer = folium.TileLayer(
+        tiles='OpenStreetMap',
+        control=False,
+    )
+    osm_layer.add_to(folium_map)
+
+    for layer in layers:
+        geojson_layer = get_geojson_layer(
+            layer=layer,
+        )
+        geojson_layer.add_to(folium_map)
+
+    folium_map.save(path)
+
+
+@dataclass
+class Layer:
+    """
+    Attributes:
+        gdf: Geodataframe
+        style: Style
+    """
+    gdf: gpd.GeoDataFrame
+    style: dict
+
+
+def compute_location(
+    layers: list[Layer],
+) -> tuple[float, float]:
+    """Computes the coordinates of the location.
+
+    Parameters:
+        layers: Layers
+
+    Returns:
+        Coordinates of the location
+    """
+    gdf = layers[0].gdf
+
+    centroid = gpd.GeoDataFrame(
+        geometry=[gdf.union_all().centroid],
+        crs=gdf.crs,
+    )
+    centroid = centroid.to_crs(epsg=4326)
+
+    return (
+        centroid.geometry.y.mean(),
+        centroid.geometry.x.mean(),
+    )
+
+
+def get_geojson_layer(
+    layer: Layer,
+) -> folium.GeoJson:
+    """Returns a GeoJSON layer.
+
+    Parameters:
+        layer: Layer
+
+    Returns:
+        GeoJSON layer
+    """
+    gdf, style = layer.gdf, layer.style
+    gdf = gdf.to_crs(epsg=4326)
+    return folium.GeoJson(
+        data=gdf,
+        style_function=lambda feature: style,  # noqa: ARG005
+        control=False,
+    )
+
+
+if __name__ == '__main__':
+    build_maps()
